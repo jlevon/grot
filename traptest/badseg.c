5cf991d39c35d075b717a1344078e96ee36aec79
John Levon <john.levon@joyent.com>
2018-07-25 11:57:59 +0000

    m

---
+++ traptest/badseg.c
@@ -0,0 +1,245 @@
+#include <setjmp.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <ucontext.h>
+#include <sys/wait.h>
+
+
+void badcs(void) { asm volatile("movw $0x1f, %dx; movw %dx, %cs"); }
+void badds(void) { asm volatile("movw $0x1f, %dx; movw %dx, %ds"); }
+void bades(void) { asm volatile("movw $0x1f, %dx; movw %dx, %es"); }
+void badfs(void) { asm volatile("movw $0x1f, %dx; movw %dx, %fs"); }
+void badgs(void) { asm volatile("movw $0x1f, %dx; movw %dx, %gs"); }
+void badss(void) { asm volatile("movw $0x1f, %dx; movw %dx, %ss"); }
+
+#define SS              18      /* only stored on a privilege transition */
+#define UESP            17      /* only stored on a privilege transition */
+#define EFL             16
+#define CS              15
+#define EIP             14
+#define ERR             13
+#define TRAPNO          12
+#define EAX             11
+#define ECX             10
+#define EDX             9
+#define EBX             8
+#define ESP             7
+#define EBP             6
+#define ESI             5
+#define EDI             4
+#define DS              3
+#define ES              2
+#define FS              1
+#define GS              0
+
+
+void resetfs(void)
+{
+	ucontext_t ucp;
+	int done = 0;
+
+	int rc = getcontext(&ucp);
+	printf("getcontext %d\n", rc);
+	printf("eip:%lx fs:%lx gs:%lx\n",
+	ucp.uc_mcontext.gregs[EIP],
+	ucp.uc_mcontext.gregs[FS],
+	ucp.uc_mcontext.gregs[GS]
+	);
+	if (done) {
+		rc = getcontext(&ucp);
+		fprintf(stderr, "done setcontext getcontext %d\n", rc);
+		fprintf(stderr, "eip:%lx fs:%lx gs:%lx\n",
+		ucp.uc_mcontext.gregs[EIP],
+		ucp.uc_mcontext.gregs[FS],
+		ucp.uc_mcontext.gregs[GS]
+		);
+		return;
+	}
+
+	done = 1;
+	ucp.uc_mcontext.gregs[FS] = 0x1f;
+	setcontext(&ucp);
+	printf("here?\n");
+}
+
+void resetgs(void)
+{
+	ucontext_t ucp;
+	int done = 0;
+
+	int rc = getcontext(&ucp);
+	printf("getcontext %d\n", rc);
+	printf("eip:%lx fs:%lx gs:%lx\n",
+	ucp.uc_mcontext.gregs[EIP],
+	ucp.uc_mcontext.gregs[FS],
+	ucp.uc_mcontext.gregs[GS]
+	);
+	if (done) {
+		rc = getcontext(&ucp);
+		fprintf(stderr, "done setcontext getcontext %d\n", rc);
+		fprintf(stderr, "eip:%lx fs:%lx gs:%lx\n",
+		ucp.uc_mcontext.gregs[EIP],
+		ucp.uc_mcontext.gregs[FS],
+		ucp.uc_mcontext.gregs[GS]
+		);
+		return;
+	}
+
+	done = 1;
+	ucp.uc_mcontext.gregs[GS] = 0x1f;
+	setcontext(&ucp);
+	printf("here?\n");
+}
+void resetcs(void)
+{
+	ucontext_t ucp;
+	int done = 0;
+
+	int rc = getcontext(&ucp);
+	printf("getcontext %d\n", rc);
+	printf("eip:%lx fs:%lx cs:%lx\n",
+	ucp.uc_mcontext.gregs[EIP],
+	ucp.uc_mcontext.gregs[FS],
+	ucp.uc_mcontext.gregs[CS]
+	);
+	if (done) {
+		rc = getcontext(&ucp);
+		fprintf(stderr, "done setcontext getcontext %d\n", rc);
+		fprintf(stderr, "eip:%lx fs:%lx cs:%lx\n",
+		ucp.uc_mcontext.gregs[EIP],
+		ucp.uc_mcontext.gregs[FS],
+		ucp.uc_mcontext.gregs[CS]
+		);
+		return;
+	}
+
+	done = 1;
+	ucp.uc_mcontext.gregs[CS] = 0x1f;
+	setcontext(&ucp);
+	printf("here?\n");
+}
+void resetds(void)
+{
+	ucontext_t ucp;
+	int done = 0;
+
+	int rc = getcontext(&ucp);
+	printf("getcontext %d\n", rc);
+	printf("eip:%lx ds:%lx gs:%lx\n",
+	ucp.uc_mcontext.gregs[EIP],
+	ucp.uc_mcontext.gregs[DS],
+	ucp.uc_mcontext.gregs[GS]
+	);
+	if (done) {
+		rc = getcontext(&ucp);
+		fprintf(stderr, "done setcontext getcontext %d\n", rc);
+		fprintf(stderr, "eip:%lx ds:%lx gs:%lx\n",
+		ucp.uc_mcontext.gregs[EIP],
+		ucp.uc_mcontext.gregs[DS],
+		ucp.uc_mcontext.gregs[GS]
+		);
+		return;
+	}
+
+	done = 1;
+	ucp.uc_mcontext.gregs[DS] = 0x1f;
+	setcontext(&ucp);
+	printf("here?\n");
+}
+void resetes(void)
+{
+	ucontext_t ucp;
+	int done = 0;
+
+	int rc = getcontext(&ucp);
+	printf("getcontext %d\n", rc);
+	printf("eip:%lx es:%lx gs:%lx\n",
+	ucp.uc_mcontext.gregs[EIP],
+	ucp.uc_mcontext.gregs[ES],
+	ucp.uc_mcontext.gregs[GS]
+	);
+	if (done) {
+		rc = getcontext(&ucp);
+		fprintf(stderr, "done setcontext getcontext %d\n", rc);
+		fprintf(stderr, "eip:%lx es:%lx gs:%lx\n",
+		ucp.uc_mcontext.gregs[EIP],
+		ucp.uc_mcontext.gregs[ES],
+		ucp.uc_mcontext.gregs[GS]
+		);
+		return;
+	}
+
+	done = 1;
+	ucp.uc_mcontext.gregs[ES] = 0x1f;
+	setcontext(&ucp);
+	printf("here?\n");
+}
+
+void resetss(void)
+{
+	ucontext_t ucp;
+	int done = 0;
+
+	int rc = getcontext(&ucp);
+	printf("getcontext %d\n", rc);
+	printf("eip:%lx fs:%lx ss:%lx\n",
+	ucp.uc_mcontext.gregs[EIP],
+	ucp.uc_mcontext.gregs[FS],
+	ucp.uc_mcontext.gregs[SS]
+	);
+	if (done) {
+		rc = getcontext(&ucp);
+		fprintf(stderr, "done setcontext getcontext %d\n", rc);
+		fprintf(stderr, "eip:%lx fs:%lx ss:%lx\n",
+		ucp.uc_mcontext.gregs[EIP],
+		ucp.uc_mcontext.gregs[FS],
+		ucp.uc_mcontext.gregs[SS]
+		);
+		return;
+	}
+
+	done = 1;
+	ucp.uc_mcontext.gregs[SS] = 0x1f;
+	setcontext(&ucp);
+	printf("here?\n");
+}
+
+void inchild(void (*func)())
+{
+	pid_t pid;
+	switch ((pid = fork())) {
+	case 0:
+		func();
+		exit(0);
+	case -1:
+		exit(1);
+	default:
+		waitpid(pid, NULL, 0);
+		return;
+	}
+
+}
+
+int main(int argc, char *argv[])
+{
+	fprintf(stderr, "resetfs\n");
+	inchild(resetfs);
+	fprintf(stderr, "resetgs\n");
+	inchild(resetgs);
+	fprintf(stderr, "resetcs\n");
+	inchild(resetcs);
+	fprintf(stderr, "resetds\n");
+	inchild(resetds);
+	fprintf(stderr, "resetes\n");
+	inchild(resetes);
+	fprintf(stderr, "resetss\n");
+	inchild(resetss);
+	//return 0;
+	inchild(badcs);
+	inchild(badds);
+	inchild(bades);
+	inchild(badfs);
+	inchild(badgs);
+	inchild(badss);
+}
