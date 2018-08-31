a4b9d8a3e0104e2b542d0556d9aba456606a687c
John Levon <john.levon@joyent.com>
2018-08-31 10:04:35 +0100

    m

---
+++ d/thread-latency.d
@@ -0,0 +1,33 @@
+#!/usr/sbin/dtrace -Cs
+
+#define ONE_SEC 1000000000
+
+#define DEBUG 0
+
+BEGIN {
+}
+
+sched:::enqueue /args[1]->pr_pid == $1/
+{
+	ts[args[0]->pr_lwpid] = timestamp;
+}
+
+sched:::on-cpu /curpsinfo->pr_pid == $1 && ts[curlwpsinfo->pr_lwpid] != 0 &&
+	(timestamp - ts[curlwpsinfo->pr_lwpid]) > (ONE_SEC / 100)/ 
+{
+	printf("thread %d (%s) now on %d was queued for %lu us", curlwpsinfo->pr_lwpid, threadname, cpu, (timestamp - ts[curlwpsinfo->pr_lwpid] ) / 1000);
+}
+
+sched:::on-cpu /curpsinfo->pr_pid == $1 && ts[curlwpsinfo->pr_lwpid] != 0/
+{
+	this->prev = ts[curlwpsinfo->pr_lwpid];
+	this->diff = timestamp - this->prev;
+	@runnable[threadname, tid] = sum(this->diff);
+	ts[curlwpsinfo->pr_lwpid] = 0;
+}
+
+tick-1s {
+	printf("run q latency sum for cpu %d", $1);
+	printa(@runnable);
+	clear(@runnable);
+}
