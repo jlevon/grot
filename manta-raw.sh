dc83c3a1eec25ae9668a03f68e8e5007d03709b7
John Levon <john.levon@joyent.com>
2019-10-28 13:40:12 +0000

    m

--- manta-raw.sh
+++ manta-raw.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+# this can only support old-style fingerprints (MD5:a0:cf:... etc)
+
 insecure="--insecure"
 alg=rsa-sha256
 #set -x
 
-#trimmed_key=$(echo $MANTA_KEY_ID | sed 's/^SHA256://')
-keyId=/$MANTA_USER/keys/$MANTA_KEY_ID
+trimmed_key=$(echo $MANTA_KEY_ID | sed 's/^SHA256://' | sed 's/^MD5://')
+keyId=/$MANTA_USER/keys/$trimmed_key
 now="date: $(date -u "+%a, %d %h %Y %H:%M:%S GMT")"
 #sig=$(echo -n $now | openssl dgst -sha256 -sign $MANTA_KEY_PATH | \
 #          openssl enc -e -a | tr -d '\n')
