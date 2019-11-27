021a4c83b83885dd87eff0f2735d7f6c988f5310
John Levon <john.levon@joyent.com>
2019-11-27 05:59:09 -0800

    m

--- create_build_zone.sh
+++ create_build_zone.sh
@@ -9,7 +9,13 @@ vnic=${name}0
 set -e
 set -x
 
-echo "create ; set zonepath=/zones/$name; set ip-type=exclusive; add net; set physical=$vnic ; end ;" | zonecfg -z $name -f -
+zonecfg -z $name -f - <<EOF
+create ;
+set zonepath=/zones/$name;
+set ip-type=exclusive;
+add net; set physical=$vnic ; end ;
+add dataset ; set name=zones/$name/export ; end ;
+EOF
 
 dladm create-vnic -l ixgbe1 -v 3307 $vnic
 zoneadm -z $name install
@@ -26,6 +32,8 @@ echo "nameserver 8.8.8.8" >/zones/$name/root/etc/resolv.conf
 echo "nameserver 8.8.4.4" >>/zones/$name/root/etc/resolv.conf
 cp /zones/$name/root/etc/nsswitch.dns /zones/$name/root/etc/nsswitch.conf
 
+zlogin $name zfs set mountpoint=/export zones/$name/export
+
 zlogin $name useradd -d /export/home/gk -P \"Primary Administrator\" gk
 mkdir -p /zones/$name/root/export/home/gk
 zlogin $name chown gk /export/home/gk
@@ -33,15 +41,23 @@ cat /zones/$name/root/etc/shadow | grep -v ^gk >/tmp/shadow.$$
 grep '^gk' /etc/shadow >>/tmp/shadow.$$
 mv /tmp/shadow.$$ /zones/$name/root/etc/shadow
 
-echo "export PATH=\$PATH:/opt/onbld/bin/:/opt/bin" >>/zones/$name/root/export/home/gk/.bash_profile
+echo "export PATH=\$PATH:/opt/onbld/bin/:/opt/bin:/usr/node/0.12/bin/:/usr/sbin:/export/home/gk/node_modules/.bin/" >>/zones/$name/root/export/home/gk/.bash_profile
+echo "export MANPATH=\$MANPATH:/opt/onbld/man:/opt/share/man" >>/zones/$name/root/export/home/gk/.bash_profile
 
 zlogin $name pkg install git exuberant-ctags build-essential runtime/python-35 developer/gcc-7 nodejs
-zlogin $name npm install -g redmine-cli
 zlogin $name npm install -g manta
 
 zlogin -l gk $name git clone https://github.com/illumos/illumos-gate.git
 
-cp /opt/bin/illumos.sh /zones/$name/root/export/home/gk/
+# redmine cli is stupid, needs config.json writable in install path
+zlogin -l gk $name npm install redmine-cli
+
+cp -r /opt/* /zones/$name/root/opt
+cp /opt/etc/illumos.sh /zones/$name/root/export/home/gk/
 zlogin $name chown gk /export/home/gk/illumos.sh
 zlogin -l gk $name cp illumos-gate/usr/src/tools/scripts/nightly.sh .
 zlogin -l gk $name chmod +x nightly.sh
+
+echo
+echo
+echo "Done: log in as gk@$ip"
