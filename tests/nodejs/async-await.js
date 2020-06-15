4dc49fe98f4ef07aaca54213c541daad7750a7c0
John Levon <levon@movementarian.org>
2020-06-15 12:08:10 +0100

    add comment

--- tests/nodejs/async-await.js
+++ tests/nodejs/async-await.js
@@ -3,6 +3,8 @@ util = require('util');
 
 const readFile = util.promisify(fs.readFile);
 
+# in Node v11.0.0 we can use ('fs').promises instead...
+
 async function etc_hosts() {
 	f = await readFile("/etc/hosts");
 	console.log(f.toString());
