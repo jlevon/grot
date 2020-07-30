3e83558b65430643c6ed40cf04f18466ceae6096
John Levon <levon@movementarian.org>
2020-07-30 09:59:26 +0100

    process-podcasts

--- home/process-podcasts.sh
+++ home/process-podcasts.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-for i in "$@"; do
+cd $1
+
+for i in *; do
 	fixed="${i%\?*}"
 	if [ "$i" != "$fixed" ]; then
 		echo mv "$i" "$fixed"
@@ -14,7 +16,12 @@ for i in "$@"; do
 done
 
 for i in MSR*; do
-set -x
 	nr=$(echo $i | sed 's+MSR++;s+_Podcast.mp3++')
 	id3v2 -a "Milk Street Radio" -A "Milk Street Radio" -g 186 -t "$nr" $i
+done
+
+for i in *.mp3; do
+	if ! id3v2 -l "$i" | grep '^TCON.*186'>/dev/null; then
+		id3v2 -g 186 "$i"
+	fi
 done
