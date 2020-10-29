da79ed6924e0048b8309080901b2cfd4d3602b77
John Levon <levon@movementarian.org>
2020-10-29 15:08:31 +0000

    sscanf-overflow

---
+++ tests/sscanf-overflow.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+#include <inttypes.h>
+
+/* sscanf can't detect integer overflow */
+
+int main()
+{
+	uint64_t out;
+	int ret;
+
+	ret = sscanf("18446744073709551616", "%"SCNu64, &out);
+	printf("ret:%d val:%" PRIu64, ret, out);
+	ret = sscanf("184467440737095516167777", "%"SCNu64, &out);
+	printf("ret:%d val:%" PRIu64, ret, out);
+}
