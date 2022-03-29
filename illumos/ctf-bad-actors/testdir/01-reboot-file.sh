878d6f513383bbd8307ea2ba93afb47ac7702d66
John Levon <john.levon@nutanix.com>
2022-03-29 15:54:16 +0000

    remove ctf-bad-actors; crap

--- illumos/ctf-bad-actors/testdir/01-reboot-file.sh
+++
@@ -1,4 +0,0 @@
-# Copyright 2013, Joyent. Inc. All rights reserved.
-
-# create a temporary file that disappears on the first reboot
-touch /tmp/.FIRST_REBOOT_NOT_YET_COMPLETE
--- illumos/ctf-bad-actors/testdir/false_encoding.py
+++
@@ -1,2 +0,0 @@
-#!/opt/local/bin/python2.7
-print '#coding=0'
