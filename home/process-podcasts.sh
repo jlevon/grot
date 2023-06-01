6132bcb6639c432ff902d42f95db98035821f72d
John Levon <levon@movementarian.org>
2023-06-01 13:55:20 +0100

    podcast processing fixes

--- home/process-podcasts.sh
+++ home/process-podcasts.sh
@@ -16,19 +16,24 @@ for i in *; do
 	if [[ "$i" = "SoundCloud%20Download" ]]; then
 		mv $i $i.$RANDOM.mp3
 	fi
+	fixed="${i%.mp3.[0-9]}"
+	if [ "$i" != "$fixed" ]; then
+		echo mv "$i" "$fixed.$RANDOM.mp3"
+		mv "$i" "$fixed.$RANDOM.mp3"
+	fi
 done
 
 for i in *.mp3; do
-	if ! mid3v2 -l "$i" | grep '^TIT2'>/dev/null; then
+	if ! mid3v2 -l "$i" | grep -a '^TIT2'>/dev/null; then
 		mid3v2 -t "${i%\.mp3}.$RANDOM" "$i"
 	fi
-	if ! mid3v2 -l "$i" | grep '^TALB'>/dev/null; then
-		mid3v2 -A "$(mid3v2 -l "$i" | grep ^TPE1 | cut -f2 -d:)" "$i"
+	if ! mid3v2 -l "$i" | grep -a '^TALB'>/dev/null; then
+		mid3v2 -A "$(mid3v2 -l "$i" | grep -a ^TPE1 | cut -f2 -d:)" "$i"
 	fi
-	if ! mid3v2 -l "$i" | grep '^TCON.*186'>/dev/null; then
+	if ! mid3v2 -l "$i" | grep -a '^TCON.*186'>/dev/null; then
 		mid3v2 -g 186 "$i"
 	fi
-	if mid3v2 -l "$i" | grep '^COMM'>/dev/null; then
+	if mid3v2 -l "$i" | grep -a '^COMM'>/dev/null; then
 		mid3v2 -c "" "$i"
 	fi
 
