b095354c7a1218f511b32789605f70d3eb7a8f78
John Levon <levon@movementarian.org>
2024-01-23 12:18:06 +0000

    add move-podcasts.sh

---
+++ home/move-podcasts.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cd ~/shared/podcasts/incoming
+
+mv Milk\Street* ../home/Milk\ Street\ Radio/Christopher\ Kimball’s\ Milk\ Street\ Radio/
+mv BBC\ Radio\ 4.The\ Kitchen\ Cabinet.* ../home/BBC\ Radio\ 4/The\ Kitchen\ Cabinet/
