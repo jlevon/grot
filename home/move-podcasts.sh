4e644745f52502dfaf8b60268997d3f40905a2ad
John Levon <levon@movementarian.org>
2024-03-23 14:44:42 +0000

    move-podcasts.sh

--- home/move-podcasts.sh
+++ home/move-podcasts.sh
@@ -2,11 +2,25 @@
 
 cd ~/shared/podcasts/incoming
 
-set -x
-mv Milk\ Street* ../home/Milk\ Street\ Radio/Christopher\ Kimball’s\ Milk\ Street\ Radio/
-mv The\ Food\ Programme.* ../home/BBC\ Radio\ 4/The\ Food\ Programme/
-mv BBC\ Radio\ 4.The\ Kitchen\ Cabinet.* ../home/BBC\ Radio\ 4/The\ Kitchen\ Cabinet/
-mv No\ Such\ Thing\ As\ A\ Fish.No\ Such\ Thing\ As\ A\ Fish.* ../home/No\ Such\ Thing\ As\ A\ Fish/No\ Such\ Thing\ As\ A\ Fish/
-mv Serious\ Eats.* ../home/Serious\ Eats/Serious\ Eats/
-mv The\ Bugle* ../home/The\ Bugle/The\ Bugle/
-mv *RA\ podcast* ../ra/
+move()
+{
+	dir=$1
+	shift
+	if [ -n "$*" ]; then
+		for f in "$@"; do
+			echo "$f"
+			mv "$f" "$dir"
+		done
+	fi
+}
+
+#set -x
+shopt -s nullglob
+
+move ../home/Milk\ Street\ Radio/Christopher\ Kimball’s\ Milk\ Street\ Radio/ Milk\ Street*
+move ../home/BBC\ Radio\ 4/The\ Food\ Programme/ The\ Food\ Programme.*
+move ../home/BBC\ Radio\ 4/The\ Kitchen\ Cabinet/ BBC\ Radio\ 4.The\ Kitchen\ Cabinet.*
+move ../home/No\ Such\ Thing\ As\ A\ Fish/No\ Such\ Thing\ As\ A\ Fish/ No\ Such\ Thing\ As\ A\ Fish.No\ Such\ Thing\ As\ A\ Fish.*
+move ../home/Serious\ Eats/Serious\ Eats/ Serious\ Eats.*
+move ../home/The\ Bugle/The\ Bugle/ The\ Bugle*
+move ../ra/ *RA\ podcast*
