454f8b20d2e855e34fb37cae180357da03d181a2
John Levon <levon@movementarian.org>
2024-06-29 18:47:59 +0100

    update move-podcasts.sh

--- home/move-podcasts.sh
+++ home/move-podcasts.sh
@@ -18,8 +18,10 @@ move()
 shopt -s nullglob
 
 move ../home/Milk\ Street\ Radio/Christopher\ Kimball’s\ Milk\ Street\ Radio/ Milk\ Street*
+move ../home/Milk\ Street\ Radio/Christopher\ Kimball’s\ Milk\ Street\ Radio/ Christopher\ Kimba*
 move ../home/BBC\ Radio\ 4/The\ Food\ Programme/ The\ Food\ Programme.*
 move ../home/BBC\ Radio\ 4/The\ Kitchen\ Cabinet/ BBC\ Radio\ 4.The\ Kitchen\ Cabinet.*
+move ../home/BBC\ Radio\ 4/The\ Kitchen\ Cabinet/ The\ Kitchen\ Cabinet.*
 move ../home/No\ Such\ Thing\ As\ A\ Fish/No\ Such\ Thing\ As\ A\ Fish/ No\ Such\ Thing\ As\ A\ Fish.No\ Such\ Thing\ As\ A\ Fish.*
 move ../home/Serious\ Eats/Serious\ Eats/ Serious\ Eats.*
 move ../home/The\ Bugle/The\ Bugle/ The\ Bugle*
